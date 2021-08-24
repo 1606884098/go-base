04684420df227c488355b0d53e5a89faedecba38
Administrator <1606884098@qq.com>
2021-08-24 19:02:32 +0800

    gc

--- gobase11_net/rpc/01-rpc-client.go
+++ gobase11_net/rpc/01-rpc-client.go
@@ -5,16 +5,21 @@ import (
 	"net/rpc"
 )
 
+//传递多个参数  理论上来讲这个是DTO，可以单独一个包，然后client server端都导入
+type AddParmaTwo struct {
+	Args1 float32 //第一个参数
+	Args2 float32 //第二个参数
+}
+
 func main() {
 	//创建客户端
 	client, err := rpc.DialHTTP("tcp", "localhost:8081")
 	if err != nil {
 		panic(err.Error())
 	}
-
 	//同步调用
-	var req float32 //请求值
-	req = 3
+	var req float32 = 3 //请求值
+	//req = 3
 	var resp *float32 //返回值
 	err = client.Call("MathUtil.CalculateCircleArea", req, &resp)
 	//err = client.Call("bieming.CalculateCircleArea", req, &resp)//别名的方式调用
@@ -30,4 +35,13 @@ func main() {
 		replayDone := <-syncCall.Done
 		fmt.Println(replayDone)
 		fmt.Println(*respSync)*/
+	var addParma AddParmaTwo
+	addParma.Args1 = 10
+	addParma.Args2 = 12
+	err = client.Call("MathUtil.Add", addParma, &resp)
+	//err = client.Call("bieming.CalculateCircleArea", req, &resp)//别名的方式调用
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(*resp)
 }
--- gobase11_net/rpc/01-rpc-server.go
+++ gobase11_net/rpc/01-rpc-server.go
@@ -16,6 +16,17 @@ func (mu *MathUtil) CalculateCircleArea(req float32, resp *float32) error {
 	return nil                  //返回类型
 }
 
+//传递多个参数
+type AddParma struct {
+	Args1 float32 //第一个参数
+	Args2 float32 //第二个参数
+}
+
+func (mu *MathUtil) Add(param AddParma, resp *float32) error {
+	*resp = param.Args1 + param.Args2 //实现两数相加的功能
+	return nil
+}
+
 func main() {
 	mathUtil := new(MathUtil) //初始化指针数据类型
 	//2、调用net/rpc包的功能将服务对象进行注册
