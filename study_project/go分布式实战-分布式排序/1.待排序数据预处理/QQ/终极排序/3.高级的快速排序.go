df6ded60f869b6d03d6321deb7e8844691b96eb2
Administrator <1606884098@qq.com>
2022-01-20 17:11:07 +0800

    排序

--- study_project/go分布式实战-分布式排序/1.待排序数据预处理/QQ/终极排序/3.高级的快速排序.go
+++ study_project/go分布式实战-分布式排序/1.待排序数据预处理/QQ/终极排序/3.高级的快速排序.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func FindindexMid(list []int, start int, end int, cur int) int {
+func FindindexMid12(list []int, start int, end int, cur int) int {
 	//对比当前位置与需要排序的元素大小，返回较大值的位置
 	if start >= end {
 		if list[start] > list[cur] {
@@ -19,19 +19,19 @@ func FindindexMid(list []int, start int, end int, cur int) int {
 
 	//二分查找递归
 	if list[mid] < list[cur] {
-		return FindindexMid(list, start, mid, cur)
+		return FindindexMid12(list, start, mid, cur)
 	} else {
-		return FindindexMid(list, mid+1, end, cur)
+		return FindindexMid12(list, mid+1, end, cur)
 	}
 
 }
-func BinSearchSort(mylist []int) []int {
+func BinSearchSort1(mylist []int) []int {
 	if len(mylist) <= 1 {
 		return mylist
 	} else {
 		for i := 1; i < len(mylist); i++ {
-			p := FindindexMid(mylist, 0, i-1, i) //0,0,  0,1,  0,2,   0,3
-			if p != i {                          //不等，插入
+			p := FindindexMid12(mylist, 0, i-1, i) //0,0,  0,1,  0,2,   0,3
+			if p != i {                            //不等，插入
 				for j := i; j > p; j-- {
 					mylist[j], mylist[j-1] = mylist[j-1], mylist[j] //数据移动
 				}
@@ -43,13 +43,13 @@ func BinSearchSort(mylist []int) []int {
 }
 
 //对制定数据段排序
-func BinSearchSortIndex(mylist []int, start int, end int) []int {
+func BinSearchSortIndex1(mylist []int, start int, end int) []int {
 	if end-start <= 1 {
 		return mylist
 	} else {
 		for i := start + 1; i <= end; i++ {
-			p := FindindexMid(mylist, start, i-1, i) //0,0,  0,1,  0,2,   0,3
-			if p != i {                              //不等，插入
+			p := FindindexMid12(mylist, start, i-1, i) //0,0,  0,1,  0,2,   0,3
+			if p != i {                                //不等，插入
 				for j := i; j > p; j-- {
 					mylist[j], mylist[j-1] = mylist[j-1], mylist[j] //数据移动
 				}
@@ -59,11 +59,11 @@ func BinSearchSortIndex(mylist []int, start int, end int) []int {
 	}
 
 }
-func QuickSortCall(arr []int) []int {
+func QuickSortCall1(arr []int) []int {
 	if len(arr) < 10 {
-		return BinSearchSort(arr)
+		return BinSearchSort1(arr)
 	} else {
-		QuickSort(arr, 0, len(arr)-1)
+		QuickSort1(arr, 0, len(arr)-1)
 		return arr
 	}
 }
@@ -72,24 +72,24 @@ func QuickSortCall(arr []int) []int {
 //123  4  697
 
 //数据交换
-func Swap(arr []int, i, j int) {
+func Swap1(arr []int, i, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
 
 //快速排序，递归
-func QuickSort(arr []int, left int, right int) {
+func QuickSort1(arr []int, left int, right int) {
 	if right-left < 10 {
-		BinSearchSortIndex(arr, left, right) //调用插入排序对于制定段排序
+		BinSearchSortIndex1(arr, left, right) //调用插入排序对于制定段排序
 	} else {
 		//快速排序写法
 		//第一个，最后一个，随机抓取
 		// 4 123  4 789  10
 		//
-		Swap(arr, left, rand.Int()%(right-left)+left) //任何一个位置，交换到第一个
-		vdata := arr[left]                            //备份中间值
-		It := left                                    // arr[left+1,lt]  <vdata  lt++
-		gt := right + 1                               //arr[gt...right]>vdata   gt--
-		i := left + 1                                 // arr[lt+1, i] ==vdata   i++
+		Swap1(arr, left, rand.Int()%(right-left)+left) //任何一个位置，交换到第一个
+		vdata := arr[left]                             //备份中间值
+		It := left                                     // arr[left+1,lt]  <vdata  lt++
+		gt := right + 1                                //arr[gt...right]>vdata   gt--
+		i := left + 1                                  // arr[lt+1, i] ==vdata   i++
 
 		//	4 7 8 9  4 1 2  3
 		//  i=1 vdata=4
@@ -108,27 +108,42 @@ func QuickSort(arr []int, left int, right int) {
 
 		for i < gt { //循环到重合
 			if arr[i] > vdata {
-				Swap(arr, i, It+1) //移动小于的地方
+				Swap1(arr, i, It+1) //移动小于的地方
 				It++
 				i++
 
 			} else if arr[i] < vdata { //吧最右边大于4的数字与最左边小于4的数交换
-				Swap(arr, i, gt-1)
+				Swap1(arr, i, gt-1)
 				gt--
 
 			} else {
 				i++ //相等
 			}
 		}
-		Swap(arr, left, It)
+		Swap1(arr, left, It)
 
-		QuickSort(arr, left, It-1) //递归处理左边
-		QuickSort(arr, gt, right)  //递归处理右边
+		QuickSort1(arr, left, It-1) //递归处理左边
+		QuickSort1(arr, gt, right)  //递归处理右边
 
 	}
 }
 
-func makearr() []int {
+func main1() {
+
+	mylist := []int{2, 1, 9, 17, 8, 10}
+	mylist = BinSearchSortIndex1(mylist, 3, 5)
+	fmt.Println(mylist)
+
+}
+func main() {
+	//mylist := makearr()
+	mylist := []int{4, 3, 8, 1, 6, 5, 7, 9, 2, 10, 11}
+	fmt.Println("start", mylist)
+	mylist = QuickSortCall1(mylist)
+	fmt.Println("end", mylist)
+}
+
+func makearr() []int { //造数组
 	var length = 100
 	var list []int
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -137,20 +152,4 @@ func makearr() []int {
 	}
 	fmt.Println(list)
 	return list
-}
-
-func main1() {
-
-	mylist := []int{2, 1, 9, 17, 8, 10}
-	mylist = BinSearchSortIndex(mylist, 3, 5)
-	fmt.Println(mylist)
-
-}
-func main() {
-
-	mylist := makearr()
-	fmt.Println("start", mylist)
-	mylist = QuickSortCall(mylist)
-	fmt.Println("end", mylist)
-
 }
